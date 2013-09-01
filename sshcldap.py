a0ca1e77f3e990270fa976cdc69029e752eb9211
Chris Swingler <chris@chrisswingler.com>
2013-09-01 15:55:29 -0500

    LDAP Class Skeleton

--- sshcldap.py
+++ sshcldap.py
@@ -1,7 +1,104 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.6
+
+"""
+sshcldap.py: A class of useful tools for working with LDAP servers.
+Tested with 389 Directory Server against the SSH:Chicago user database;
+probably works with other directories, but you'll have to try to find
+out. :-)
+"""
 
 __author__ = "Christopher Swingler"
 __copyright__ = "Copyright 2013, South Side Hackerspace Chicago"
 __license__ = "tbd"
 __email__ = "chris@chrisswingler.com"
 __status__ = "Development"
+
+import ldap
+
+class sshcldap:
+    """
+    A class with LDAP tools for interacting with our repository.
+    """
+
+    BASEDN="dc=sshchicago,dc=org"
+    URL="ldap://dir.sshchicago.org:389"
+
+    STANDARD_USER_GROUPS=[('cn','SSHC Members')]
+    ADMIN_USER_GROUPS=[('cn','Administrative Members')] + STANDARD_USER_GROUPS
+    OFFICER_USER_GROUPS=[('cn','Officers')] + ADMIN_USER_GROUPS
+
+    __lconn = None
+
+    def __init__(self, uid, password, url, basedn=None):
+        """
+        Initializes the connection using uid and password. 
+        A fully qualified cn is constructed by combining uid and basedn.
+        """
+        if (basedn != None):
+            self.basedn = basedn
+
+
+     def __delete__(self):
+         """
+         Deconstructor, just unbinds the connection.
+         """
+         try:
+             __lconn.unbind()
+         except:
+             # Eh, who cares.
+             pass
+
+     def create_user(self, givenName, sn, mail, uid=None, password=None):
+         """
+         Creates a user under basedn. If not specified, uid will be 
+         the first letter of givenName + sn (lower case), and
+         password will be randomly generated.
+
+         Retruns a tuple containing (uid, password); password in cleartext.
+         """
+         pass
+
+     def delete_user(self, uid):
+         """
+         Deletes a user. Should not be used much, in favor of deactivate_user.
+         """
+         pass
+
+     def deactivate_user(self, uid):
+         """
+         Deactivates a user; preventing them from logging in. Leaves the entry
+         in the database.
+         """
+         pass
+
+     def add_to_groups(self, uid, groups):
+         """
+         Adds the uid to the list of groups.
+         """
+         pass
+
+     def set_standard_user_groups(self, uid):
+         """
+         Adds the uid to the list of standard groups defined by 
+         STANDARD_USER_GROUPS
+         """
+         pass
+
+    def set_admin_user_groups(self, uid):
+        """
+        Adds the uid to the list of groups defined by
+        ADMIN_USER_GROUPS
+        """
+        pass
+
+    def set_officer_user_groups(self, uid):
+        """
+        Adds the uid to the list of groups defined by
+        OFFICER_USER_GROUPS
+        """
+        pass
+
+    def reset_password(self, uid):
+        """
+        Sets the password of uid to a random value. Returns a tuple of uid, new password. 
+        """
