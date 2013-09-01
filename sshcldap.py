98fd0853de30aadd5de1902ceede1d4c21f2757f
Chris Swingler <chris@chrisswingler.com>
2013-09-01 15:51:11 -0500

    Wrote a simple class skeleton for talking to the ldap server

---
+++ sshcldap.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+__author__ = "Christopher Swingler"
+__copyright__ = "Copyright 2013, South Side Hackerspace Chicago"
+__license__ = "tbd"
+__email__ = "chris@chrisswingler.com"
+__status__ = "Development"
