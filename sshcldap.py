c6f85e728975a4f5d055caae543cc309311d17c7
Chris Swingler <chris@chrisswingler.com>
2013-09-01 16:20:47 -0500

    User binding, and initial test

--- sshcldap.py
+++ sshcldap.py
@@ -29,20 +29,20 @@ class sshcldap:
 
     __lconn = None
 
-     def __init__(self, uid, password, url=None, basedn=None):
-        """
-        Initializes the connection using uid and password. 
-        A fully qualified cn is constructed by combining uid and basedn.
-        """
-        if (basedn != None):
-            self.BASEDN = basedn
-        if (url != None):
-            self.URL = url
+    def __init__(self, uid, password, url=None, basedn=None):
+         """
+         Initializes the connection using uid and password. 
+         A fully qualified cn is constructed by combining uid and basedn.
+         """
+         if (basedn != None):
+             self.BASEDN = basedn
+             if (url != None):
+                 self.URL = url
 
-        self.__lconn = ldap.initialize(self.URL)
-        self.__lconn.simple_bind_s(self.__fquid(uid), password)
+         self.__lconn = ldap.initialize(self.URL)
+         self.__lconn.simple_bind_s(self.__fquid(uid), password)
 
-     def __delete__(self):
+    def __delete__(self):
          """
          Deconstructor, just unbinds the connection.
          """
@@ -52,25 +52,25 @@ class sshcldap:
              # Eh, who cares.
              pass
 
-     def __fquid(self, uid):
+    def __fquid(self, uid):
          """
          Returns the fully-qualified dn of a uid.
          """
          return "uid=%s,ou=people,%s" % (uid, self.BASEDN)
 
-     def is_connection_valid(self):
+    def is_connection_valid(self):
          """
          Checks if the LDAP bind is working. Returns true/false.
          """
          # Ideally, we'd use python-ldap's whoami_s() to see who we are and if we've
          # bound, but 389 doesn't implement RFC 4532. In that case, we're
          # going to do a search, and if we get more than 1 result, consider it good.
-         r = __lconn.search_s(self.BASEDN, ldap.SCOP_SUBTREE, '(cn=*)',['mail'])
+         r = self.__lconn.search_s(self.BASEDN, ldap.SCOPE_SUBTREE, '(cn=*)',['mail'])
          if (len(r) > 1):
              return True
          return False
 
-     def create_user(self, givenName, sn, mail, uid=None, password=None):
+    def create_user(self, givenName, sn, mail, uid=None, password=None):
          """
          Creates a user under basedn. If not specified, uid will be 
          the first letter of givenName + sn (lower case), and
@@ -80,26 +80,26 @@ class sshcldap:
          """
          pass
 
-     def delete_user(self, uid):
+    def delete_user(self, uid):
          """
          Deletes a user. Should not be used much, in favor of deactivate_user.
          """
          pass
 
-     def deactivate_user(self, uid):
+    def deactivate_user(self, uid):
          """
          Deactivates a user; preventing them from logging in. Leaves the entry
          in the database.
          """
          pass
 
-     def add_to_groups(self, uid, groups):
+    def add_to_groups(self, uid, groups):
          """
          Adds the uid to the list of groups.
          """
          pass
 
-     def set_standard_user_groups(self, uid):
+    def set_standard_user_groups(self, uid):
          """
          Adds the uid to the list of standard groups defined by 
          STANDARD_USER_GROUPS
--- test_sshcldap.py
+++ test_sshcldap.py
@@ -1,3 +1,33 @@
 #!/usr/bin/env python2.6
 
 # Tests for the sshcldap class.
+
+# Depends on a file named "ldapcreds.cfg" being in the current working
+# directory, formatted as:
+#   [ldap]
+#   user=eidle
+#   passwd=spam
+
+
+import unittest
+from ConfigParser import RawConfigParser as cparser
+import sshcldap
+
+config = cparser()
+config.read('ldapcreds.cfg')
+
+uid=config.get('ldap','user')
+passwd=config.get('ldap','passwd')
+
+l = sshcldap.sshcldap(uid, passwd)
+
+class ldapUnitTests(unittest.TestCase):
+
+    def testBind(self):
+        self.failUnless(l.is_connection_valid())
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
