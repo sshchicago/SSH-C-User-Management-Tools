8d050e6e840289b2d124c44b298570fb62542ed1
Chris Swingler <chris@chrisswingler.com>
2013-12-01 16:09:12 -0600

    Fixed some terrible bugs, added some logging

--- mass_sshc_account_create.py
+++ mass_sshc_account_create.py
@@ -19,10 +19,14 @@ __status__ = "Development"
 import sys
 import csv
 import logging
-import config
 import sshcldap
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+logger.addHandler(ch)
 
 class ldap_account_manager:
 
@@ -30,8 +34,7 @@ class ldap_account_manager:
     ldapuser = None
     ldappass = None
     ldapServerUrl = None
-    l = None
-
+#    l = None
 
     # Each list that we're playing with
     toAddList = []
@@ -55,10 +58,11 @@ class ldap_account_manager:
         self.ldappass = password
         self.ldapserver = serverUrl
 
-        self.l = sshcldap.sshcldap(self.ldapuser, self.ldapppass, self.ldapServerUrl)
-        logger.debug("Created a LDAP server connection. Status is %s" % str(l.is_connection_valid))
+        self.l = sshcldap.sshcldap(self.ldapuser, self.ldappass, self.ldapServerUrl)
+        logger.debug("Created a LDAP server connection. Status is %s" % str(self.l.is_connection_valid()))
         # May as well populate the exiting_user_list, it's not going to change until we're done.
         self.existingUserList = self.l.list_people()
+        logger.debug("existingUserList contains %s people " % len(self.existingUserList))
     
     def __does_user_exist(self, email):
         """
@@ -67,8 +71,8 @@ class ldap_account_manager:
         email = email.lower()
         for user in self.existingUserList:
             if user[1]['mail'].lower() == email:
-                return true
-        return false
+                return True
+        return False
 
     def build_change_lists(self, csvname):
         """
@@ -80,6 +84,7 @@ class ldap_account_manager:
             * Protected users (would have been deactivated but are somehow excepted)
                 toProtectList
         """
+        self.populate_paying_users(csvname)
         # For user in active users:
             # Check against each row in payingAccount.
             # If not there, add to deActivateList
@@ -88,97 +93,105 @@ class ldap_account_manager:
             # If active, add to toUnchangedList
             # If inactive, add to toReactivateList
         for existingUser in self.existingUserList:
-            if self.__paying_user(existingUser[1]['mail']) == true:
-                if (l.is_user_active(existingUser[1]['uid']) == true):
+            if self.__paying_user(existingUser[1]['mail']) == True:
+                if (self.l.is_user_active(existingUser[1]['uid']) == True):
                     # Existing, active. Do nothing.
                     self.toUnchangedList += existingUser
                     break
-                if (l.is_user_active(existingUser[1]['uid']) == false):
+                if (self.l.is_user_active(existingUser[1]['uid']) == False):
                     # An inactive user paid thier dues! Activate them.
                     self.toReactivateList += existingUser
                     break
             else:
-                # We've made it through the paying user list without 
-                if (l.is_member_of_group(existingUser[1]['uid'], l.PROTECTED_USER_GROUPS[0])):
+                # We've made it through the paying user list without finding them.
+                if (self.l.is_member_of_group(existingUser[1]['uid'], self.l.PROTECTED_USER_GROUPS[0])):
                     # User is protected, put them in that list.
                     self.toProtectList += existingUser
                     break
                 else:
                     # Someone didn't pay their dues. Deactivate them if they're inactive, 
                     # otherwise do nothing.
-                    if (l.is_user_active(existingUser[1]['uid'] == true)):
+                    if (self.l.is_user_active(existingUser[1]['uid'] == True)):
                         self.toDeactivateList += existingUser
                     else:
                         self.toUnchangedList += existingUser
 
-
-
-                
-        
         # For user in paying users:
-            # Check against each Active User.
-            # If present, do nothing.
-            # If not present, add them to toAddList
+        # Check against each Active User.
+        # If present, do nothing.
+        # If not present, add them to toAddList
+        for payingUser in self.payingUserList:
+            logger.debug("Going to do things with %s" % payingUser)
+            pass
+
 
 
     def populate_paying_users(self, csvfile):
         """
         Populates self.payingUsersList
         """
-        infile = open(csvname,'r')
+        infile = open(csvfile,'r')
         csvreader = csv.DictReader(infile)
         for row in csvreader:
+            logger.debug("Parsing row %s " % row)
             if row['Status'] == 'Active':
+                logger.debug("Found active user %s " % row)
                 userAccount = {}
-                profileId = row['Profile ID']
-                description = row['Description']
-                payerName = row['PayerName']
-                mail = row['Email']
-                status = row['Status']
-                amountLast = row['Amount Last']
-                nextBillDate = row['Next Bill Date']
-                self.payingUserList += userAccount
+                userAccount['profileId'] = row['Profile ID']
+                userAccount['description'] = row['Description']
+                userAccount['payerName'] = row['Payer Name']
+                userAccount['mail'] = row['Email']
+                userAccount['status'] = row['Status']
+                userAccount['amountLast'] = row['Amount Last Paid']
+                userAccount['nextBillDate'] = row['Next Bill Date']
+                self.payingUserList.append(userAccount)
+        logger.debug("Paying users is %s " % self.payingUserList)
 
     def __paying_user(self, email):
         """
         Checks if email matches against an entry in self.payingUserList
         """
         for user in self.payingUserList:
-            if user['mail'].lower() == email.lower():
-                return true
-        return false
+            logger.debug("Checking email %s against paying user %s " % (email,user))
+            if user['mail'].lower() == email[0].lower():
+                return True
+        return False
 
-    def create_user(self, givenName, surname, emailAddr, doEmail = true):
+    def create_user(self, givenName, surname, emailAddr, doEmail = True):
         """
         Creates a new user and puts them in the default groups.
-        Sends out an email to them about their new account (unless doEmail = false)
+        Sends out an email to them about their new account (unless doEmail = False)
         """
+        logger.debug("create_user: %s | %s | %s | %s" % (givenName, surname, emailAddr, doEmail ))
         pass
 
-    def deactivate_user(self, uid, doEmail = false, specialReason = None):
+    def deactivate_user(self, uid, doEmail = False, specialReason = None):
         """
         Deactivates an existing user. 
         Sends out an email to tell them that their account has been deactivated
         if doEmail = True, and allows a special reason to be attached by passing specialReason
-        Returns true if a deactivation worked. Returns false if the user is already deactivated.
+        Returns True if a deactivation worked. Returns False if the user is already deactivated.
         Skips doEmail if activation state doesn't change.
         """
+        logger.debug("deactivate_user: %s | %s | %s" % (uid, doEmail, specialReason))
         pass
 
-    def activate_user(self, uid, doEmail = false, specialReason = None):
+    def activate_user(self, uid, doEmail = False, specialReason = None):
         """
         Reactivates a previously deactivated user.
         Sends out an email to tell them that their account has been reactivated
         if doEmail = True, and allows for a reason.
-        Returns true if an activation worked. Returns false if the user is already active
+        Returns True if an activation worked. Returns False if the user is already active
         Skips doWmail if activation state hasn't changed.
         """
+        logger.debug("activate_user: %s | %s | %s" % (uid, doEmail, specialReason))
         pass
 
     def __send_mail(self, toAddress, subject, body, ccAddress=None, bccAddress=None):
         """
         Sends out an email.
         """
+        logger.debug("__send_email: %s | %s | %s | %s | %s" % (toAddress, subject, body, ccAddress, bccAddress))
         pass
 
 
