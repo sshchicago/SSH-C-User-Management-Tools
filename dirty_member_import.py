f729ea9edd84d25d5140772aaff0e94bb1858050
Chris Swingler <chris@chrisswingler.com>
2013-12-13 11:42:53 -0600

    A really simple LDIF generator.

---
+++ dirty_member_import.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# A really hackish way to add LDAP members.
+# Takes a CSV with this format:
+# FullName, EmailAddress, Password (expects headers 
+# EmailAddress can be blank.
+# Outputs an LDIF to stdout that can then be used by ldapadd or ldapmodify.
+
+import csv
+import sys
+from time import strftime
+
+csvfile = sys.argv[1]
+
+with open(csvfile, 'rU') as csvfile:
+    member_rdr = csv.reader(csvfile)
+    for row in member_rdr:
+        cn=row[0]
+        mail=row[1]
+        userpassword=row[2]
+        givenname=cn.split()[0]
+        sn=cn.split()[1]
+        uid=givenname[0].lower() + sn.lower()
+        template="""dn: uid=%s,ou=People,dc=sshchicago,dc=org
+objectclass: top
+objectclass: person
+objectclass: inetorgperson
+cn: %s
+sn: %s
+givenname: %s
+mail: %s
+userpassword: %s
+"""
+        userldif = template % (uid, cn, sn, givenname, mail, userpassword)
+        print userldif
